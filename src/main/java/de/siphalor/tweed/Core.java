1f0bb816162a9726a0fee8a06a5265a1fb7d7094
Siphalor <xyx0125@gmail.com>
2019-03-30 13:05:59 +0100

    Add logos, fixes and cleanup

--- src/main/java/de/siphalor/tweed/Core.java
+++ src/main/java/de/siphalor/tweed/Core.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class Core {
 	public static final String MODID = "tweed";
 	public static final char HJSON_PATH_DELIMITER = ':';
-	public static final String mainConfigDirectory = FabricLoader.getInstance().getConfigDirectory().getAbsolutePath() + File.separator + MODID + File.separator;
+	public static final String mainConfigDirectory = FabricLoader.getInstance().getConfigDirectory().getAbsolutePath() + File.separator;
 	public static final HjsonOptions HJSON_OPTIONS = new HjsonOptions().setAllowCondense(false).setBracesSameLine(true).setOutputComments(true).setSpace("\t");
 
 }
