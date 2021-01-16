22a22412f3c78ac04143f97f2aba88aa4c3e646d
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-01-16 18:06:57 +0100

    use setErrorSupplier in IntSliderConverter

--- src/main/java/de/siphalor/tweed/client/cloth/IntSliderConverter.java
+++ src/main/java/de/siphalor/tweed/client/cloth/IntSliderConverter.java
@@ -4,7 +4,10 @@ import de.siphalor.tweed.config.entry.ValueConfigEntry;
 import de.siphalor.tweed.tailor.ClothTailor;
 import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
+
+import java.util.Optional;
 
 public class IntSliderConverter implements ClothTailor.EntryConverter<Integer>{
     final int min, max;
@@ -19,6 +22,11 @@ public class IntSliderConverter implements ClothTailor.EntryConverter<Integer>{
         return builder.startIntSlider(new TranslatableText(s), entry.getMainConfigValue(), min, max)
             .setDefaultValue(entry.getDefaultValue())
             .setSaveConsumer(entry::setMainConfigValue)
+            .setErrorSupplier((Integer value) -> {
+                if (value < min || value > max)
+                    return Optional.of(new LiteralText(String.format("value should be between %d and %d", min, max)));
+                return Optional.empty();
+            })
             .build();
     }
 }
